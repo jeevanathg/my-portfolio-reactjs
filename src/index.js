ce7e5a75b5242aa14c84b654c2d3e816e2f1a4c2
Jeevanath Gopalakrishnan <jeevanath.g@gmail.com>
2022-07-17 13:45:42 +0530

    Initial CSS with Index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import react from "react";
 import ReactDOM, { render } from "react-dom";
 import App from "./App";
+import './index.css'
 
 
 ReactDOM.render(<App/>, document.getElementById("root"))
